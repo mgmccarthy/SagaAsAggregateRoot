a84af0f9430fdcc26c434472699f8ffb67be1bfb
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-10-23 12:05:50 -0400

    add SiteSupplyIsBelowResupplyThresholdHandler which log warns to console

---
+++ SagaAsAggregateRoot.Endpoint/Handlers/SiteSupplyIsBelowResupplyThresholdHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using SagaAsAggregateRoot.Shared.Messages.Events;
+
+namespace SagaAsAggregateRoot.Endpoint.Handlers
+{
+    public class SiteSupplyIsBelowResupplyThresholdHandler : IHandleMessages<SiteSupplyIsBelowResupplyThreshold>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<SiteSupplyIsBelowResupplyThresholdHandler>();
+
+        public Task Handle(SiteSupplyIsBelowResupplyThreshold message, IMessageHandlerContext context)
+        {
+            Log.Warn($"Site supply has fallen below resupply threshold, remaining supply: {message.AvailableQuantity}");
+            return Task.CompletedTask;
+        }
+    }
+}
